7460f6cd1a75ca1882805d25da93bd933cefbba8
cdelashm <cdelashm@bc5d0606-792e-0410-9700-c12c62b75ab7>
2007-04-24 04:14:55 +0000

    Separating core and example war into separate directories, and adding in annotation based event handling

--- Richa/core/src/org/richa/runner/RichaRunnerServlet.java
+++ Richa/core/src/org/richa/runner/RichaRunnerServlet.java
@@ -2,17 +2,34 @@ package org.richa.runner ;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.richa.event.EventListenerMetaData;
+import org.richa.scanner.EventListenerScanner;
 import org.richa.util.AppendingStringBuffer;
 
+@SuppressWarnings("serial")
 public class RichaRunnerServlet extends HttpServlet
 {
+
+	private Map<String,EventListenerMetaData>eventListeners; 
+
+	@Override
+	public void init() throws ServletException
+	{
+		super.init();
+		
+		//Find event handlers
+		eventListeners = new EventListenerScanner("simpli.xml").getClasses();
+	}
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		PrintWriter pw = null ;
@@ -64,6 +81,36 @@ public class RichaRunnerServlet extends HttpServlet
 			if (pw != null)
 				pw.close() ;
 		}
+	}
+	
+	/**
+	 * Handles event posts from the UI.
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		//Lookup a class to handle request. Use the event name which is passed after the servlet name
+		try
+		{
+			//Events come into the servlet as <eventListner>/<eventHandler>
+			String eventData = request.getPathInfo().substring(1);
+			String eventListner = eventData.substring(0,eventData.indexOf("/"));
+			String eventHandler = eventData.substring(eventData.indexOf("/")+1);
+			
+			Class eventListenerClass = eventListeners.get(eventListner).getEventListener();
+			
+			Object eventListener = eventListenerClass.newInstance();
+			
+			//Find the method that is associated with the eventName
+			Method eventMethod = eventListeners.get(eventListner).getEventHandler(eventHandler);
+			
+			response.getWriter().println(eventMethod.invoke(eventListener));
+		}
+		catch(Throwable e)
+		{
+			throw new ServletException(e);
+		}
+		
 	}
 	
 	/**
---
+++ Richa/core/src/org/richa/scanner/EventListenerScanner.java
@@ -0,0 +1,119 @@
+package org.richa.scanner;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javassist.bytecode.ClassFile;
+
+import org.richa.annotations.EventHandler;
+import org.richa.annotations.EventListener;
+import org.richa.event.EventListenerMetaData;
+
+public class EventListenerScanner extends Scanner
+{
+	private static final Logger log = Logger
+			.getLogger(EventListenerScanner.class.getName());
+
+	private Map<String, EventListenerMetaData> classes;
+
+	public EventListenerScanner(String resourceName)
+	{
+		super(resourceName);
+	}
+
+	public EventListenerScanner(String resourceName, ClassLoader classLoader)
+	{
+		super(resourceName, classLoader);
+	}
+
+	/**
+	 * Returns only Simpli Event Handlers (ie: classes annotated with
+	 * 
+	 * @EventHandler)
+	 */
+	public Map<String, EventListenerMetaData> getClasses()
+	{
+		if (classes == null)
+		{
+			classes = new HashMap<String, EventListenerMetaData>();
+			scan();
+		}
+		return classes;
+	}
+
+	@Override
+	protected void handleItem(String name)
+	{
+		if (name.endsWith(".class"))
+		{
+			String classname = filenameToClassname(name);
+			try
+			{
+				ClassFile classFile = getClassFile(name);
+				boolean installable = hasAnnotation(classFile,
+						EventListener.class);
+				if (installable)
+				{
+					String eventListenerName = getEventName(classFile);
+					if (log.isLoggable(Level.FINE))
+						log.fine("found event handler class: " + name
+								+ " for event listener: " + eventListenerName);
+					// Find all annotated methods
+					Class clazz = classLoader.loadClass(classname);
+					EventListenerMetaData elmd = new EventListenerMetaData(clazz);
+					
+					for(Method method : clazz.getMethods())
+					{
+						EventHandler eh = method.getAnnotation(EventHandler.class);
+						if(eh != null)
+						{
+							String eventHandlerName = eh.value();
+							if(eventHandlerName.equals(EventHandler.DEFAULT_EVENT))
+							{
+								elmd.addEventHandler(method.getName(), method);
+							}
+							else
+							{
+								elmd.addEventHandler(eventHandlerName, method);
+							}
+						}
+					}
+					
+					classes.put(eventListenerName, elmd );
+				}
+			}
+			catch (ClassNotFoundException cnfe)
+			{
+				log.log(Level.FINE, "could not load class: " + classname, cnfe);
+
+			}
+			catch (NoClassDefFoundError ncdfe)
+			{
+				log.log(Level.FINE,
+						"could not load class (missing dependency): "
+								+ classname, ncdfe);
+
+			}
+			catch (IOException ioe)
+			{
+				log.log(Level.FINE, "could not load classfile: " + classname,
+						ioe);
+			}
+		}
+	}
+
+	private String getEventName(ClassFile classFile)
+	{
+		String name = getAnnotationValue(classFile, EventListener.class,
+				"value");
+		if (name == null)
+		{
+			name = classFile.getName();
+		}
+		return name;
+	}
+}
