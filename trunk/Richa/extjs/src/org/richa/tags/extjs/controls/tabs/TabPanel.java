e9c57d350239d2da1b969224078f76aa6cc95d29
richa.ramv <richa.ramv@bc5d0606-792e-0410-9700-c12c62b75ab7>
2007-04-25 03:49:25 +0000

    Changes

--- trunk/Richa/extjs/src/org/richa/tags/extjs/controls/tabs/TabPanel.java
+++ trunk/Richa/extjs/src/org/richa/tags/extjs/controls/tabs/TabPanel.java
@@ -25,12 +25,44 @@ public class TabPanel extends BaseExtJSTag
 	 */
 	protected void beforeBody(final XMLOutput output) throws JellyTagException, SAXException
 	{
+		//Does it have a valid name
 		String name = getName() ;
 		if (isEmpty(name))
-			throw new JellyTagException("Name is a required parameter for a form tag") ;
+			throw new JellyTagException("name is a required parameter for this tag") ;
+		
+		//Is there another tab tag open
+		if (!isEmpty(getCurrentTabPanelName()))
+			throw new JellyTagException("TabPanels cannot be embedded") ;
+		
+		//Generate th div for the tab panel
+		output.write("<div id=\"" + "tab-" + name + "-id" + "\">\n") ;
 		
 		//Generate a tab Panel creation
 		serializeCreation() ;
+		
+		//Set this tab panel as the current tab panel
+		setCurrentTabPanelName(name) ;
+	}
+	
+	/**
+	 * Generate html and js after the body is processed
+	 */
+	protected void afterBody(final XMLOutput output) throws JellyTagException, SAXException
+	{	
+		output.write("</div>\n") ;
+		
+	    //Clear the current tab panel name
+	    clearCurrentTabPanelName() ;
+	}   
+	
+	//Create the tab panel
+	protected void serializeCreation()
+	{
+		scriptBuffer.appendln("    var " + getName() + " = new " + getObjectName() + "(\"tab-" + getName() + "-id\", {");
+		
+		//Serialize all the attributes
+		serializeAttributes() ;
+		
+		scriptBuffer.appendln("    });") ;
 	}
 }
-
